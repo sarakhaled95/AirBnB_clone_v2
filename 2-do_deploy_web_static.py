1c493573200d0a816a7c075705dc0fc8f95b39bd
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-10 10:45:04 -0800

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,3 +3,28 @@
 distributes an archive to your web servers, using the function
 """
 
+
+from fabric.api import
+from os.path import exists
+env.hosts = ['54.157.145.55', '100.25.139.164']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers"""
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        return True
+    except:
+        return False

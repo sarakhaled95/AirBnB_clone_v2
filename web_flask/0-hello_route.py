6ab396be88ae47db4b081023ce18fbd5fcafac20
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-25 18:15:48 +0200

    3

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,8 @@ web_flask = Flask(__name__)
 
 @web_flask.route("/", strict_slashes=False)
 def hello_route():
-    return "Hello HBNB!"
+    """ hello method """
+    return ("Hello HBNB!")
 
 if __name__ == "__main__":
     web_flask.run(host='0.0.0.0', port=5000)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+
+from flask import Flask
+
+web_flask = Flask(__name__)
+
+@web_flask.route("/", strict_slashes=False)
+def hello_route():
+    """ hello method """
+    return ("Hello HBNB!")
+
+@web_flask.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """ hbnb method """
+    return ("HBNB")
+
+@web_flask.route("/c/<text>", strict_slashes=False)
+def C_route(text):
+    """ c method """
+    text = text.relace('_', ' ')
+    return ('C' + ' ' + text)
+
+@web_flask.route("/python/<text>", strict_slashes=False)
+def python_route(text=None):
+    """ python method """
+    if text is None:
+        text = 'is cool'
+    else:
+        text = text.relace('_', ' ')
+    return ('python' + ' ' + text)
+
+if __name__ == "__main__":
+    web_flask.run(host='0.0.0.0', port=5000)
